a0ebb23b6476f07fc4658209f41a8b07989a92c6
Ayane Sok <ayane.sok@mrjaffesclass.com>
2023-09-14 11:45:28 -0700

    commit

---
+++ array.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class array here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class array
+{
+    public static void main(String[] args)
+    {
+        String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
+        for (int i=0; i < strArray.length; i++) {
+        System.out.println(strArray[i]); 
+    }
+        for (String array : strArray) {
+            System.out.print(array);
+    }
+        for (String array : strArray) {
+            System.out.print(array); 
+            
+           
+        }
+        
+    }
+}
+
